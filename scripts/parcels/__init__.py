6264895541a011714ec1ece8e6b84cfaee36f7f7
Andrew Scherer <ascherer97@gmail.com>
2025-03-12 16:27:15 -0700

    copy of OceanParcels code since most recent version is not on PyPi yet

---
+++ scripts/parcels/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "3.1.2.dev0"
+
+import parcels.rng as ParcelsRandom  # noqa: F401
+from parcels.application_kernels import *
+from parcels.field import *
+from parcels.fieldset import *
+from parcels.grid import *
+from parcels.gridset import *
+from parcels.interaction import *
+from parcels.kernel import *
+from parcels.particle import *
+from parcels.particlefile import *
+from parcels.particleset import *
+from parcels.tools import *
